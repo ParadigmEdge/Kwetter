5d32949ac98e08661f380975509df282ad467144
yongyi <yong-yi@live.com>
2015-03-17 12:24:58 +0100

    work in progress

--- public_html/app/angular/Services.js
+++ public_html/app/angular/Services.js
@@ -1,13 +1,9 @@
 angular.module("kwetterApp.services",[]).factory("User", function($resource){
     return $resource('http://localhost:23761/Kwetter_Server/api/kwetter/users/:id',{id:'@id'},{
-        update: {
-            method: 'PUT'
-        }
+        update: {method: 'PUT'}
     });
 }).factory("Tweet", function($resource){
     return $resource('http://localhost:23761/Kwetter_Server/api/kwetter/tweets/:id',{id:'@id'},{
-        update: {
-            method: 'PUT'
-        }
+        update: {method: 'PUT'}
     });
 });
